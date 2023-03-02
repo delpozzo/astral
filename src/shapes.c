5da1c069bbfc6fd3b29ff44492c575478a4fa673
delpozzo <1431725+delpozzo@users.noreply.github.com>
2023-03-02 03:11:32 +0000

    More shapes

--- src/shapes.c
+++ src/shapes.c
@@ -1,7 +1,8 @@
 // ###########################################################################
 //          Title: Astral Shape Functions
 //         Author: Mike Del Pozzo
-//    Description: Functions to draw shapes such as rectangles and circles.
+//    Description: Functions to draw basic shapes such as rectangles,
+//                 circles, and lines.
 //        Version: 0.2.0
 //           Date: 24 Feb 2023
 //        License: GPLv3 (see LICENSE)
@@ -16,6 +17,8 @@
 #include "shapes.h"
 
 extern SDL_Renderer *gRenderer;
+
+SDL_Color rainbowColor;
 
 void drawRect(SDL_Rect *rect, SDL_Color *color, bln fill)
 {
@@ -36,6 +39,18 @@ void drawRect(SDL_Rect *rect, SDL_Color *color, bln fill)
     }
 }
 
+void drawRainbowRect(SDL_Rect *rect, u8 speed, bln fill)
+{
+    if(SDL_GetTicks() % speed == 0)
+    {
+        rainbowColor.r = rand() % 255;
+        rainbowColor.b = rand() % 255;
+        rainbowColor.g = rand() % 255;
+    }
+
+    drawRect(rect, &rainbowColor, fill);
+}
+
 void drawCircle(i32 x0, i32 y0, i32 radius, SDL_Color *color)
 {
     if(gRenderer == NULL || color == NULL)
@@ -45,12 +60,13 @@ void drawCircle(i32 x0, i32 y0, i32 radius, SDL_Color *color)
 
     SDL_SetRenderDrawColor(gRenderer, color->r, color->g, color->b, color->a);
 
-    i32 x = 0, y = radius;
+    i32 x = 0;
+    i32 y = radius;
     i32 dp = 1 - radius;
 
     do
     {
-        if (dp < 0)
+        if(dp < 0)
         {
             dp = dp + 2 * (++x) + 3;
         }
@@ -68,5 +84,41 @@ void drawCircle(i32 x0, i32 y0, i32 radius, SDL_Color *color)
         SDL_RenderDrawPoint(gRenderer, x0 + y, y0 - x);
         SDL_RenderDrawPoint(gRenderer, x0 - y, y0 - x);
     } while (x < y);
+}
+
+void drawRainbowCircle(i32 x0, i32 y0, i32 radius, u8 speed)
+{
+    if(SDL_GetTicks() % speed == 0)
+    {
+        rainbowColor.r = rand() % 255;
+        rainbowColor.b = rand() % 255;
+        rainbowColor.g = rand() % 255;
+    }
+
+    drawCircle(x0, y0, radius, &rainbowColor);
+}
+
+void drawLine(i32 x1, i32 y1, i32 x2, i32 y2, SDL_Color *color)
+{
+    if(gRenderer == NULL || color == NULL)
+    {
+        return;
+    }
+
+    SDL_SetRenderDrawColor(gRenderer, color->r, color->g, color->b, color->a);
+
+    SDL_RenderDrawLine(gRenderer, x1, y1, x2, y2);   
+}
+
+void drawRainbowLine(i32 x1, i32 y1, i32 x2, i32 y2, u8 speed)
+{
+    if(SDL_GetTicks() % speed == 0)
+    {
+        rainbowColor.r = rand() % 255;
+        rainbowColor.b = rand() % 255;
+        rainbowColor.g = rand() % 255;
+    }
+
+    drawLine(x1, y1, x2, y2, &rainbowColor);
 }
 
--- src/shapes.h
+++ src/shapes.h
@@ -1,7 +1,8 @@
 // ###########################################################################
 //          Title: Astral Shape Functions
 //         Author: Mike Del Pozzo
-//    Description: Functions to draw shapes such as rectangles and circles.
+//    Description: Functions to draw basic shapes such as rectangles,
+//                 circles, and lines.
 //        Version: 0.2.0
 //           Date: 24 Feb 2023
 //        License: GPLv3 (see LICENSE)
@@ -21,8 +22,20 @@
 // Draw a rectangle (filled or unfilled)
 void drawRect(SDL_Rect *rect, SDL_Color *color, bln fill);
 
+// Same as above, but with random generated colors
+void drawRainbowRect(SDL_Rect *rect, u8 speed, bln fill);
+
 // Draw a circle at center (x0, y0) with specified radius
 void drawCircle(i32 x0, i32 y0, i32 radius, SDL_Color *color);
+
+// Same as above, but with random generated colors
+void drawRainbowCircle(i32 x0, i32 y0, i32 radius, u8 speed);
+
+// Draw a line from (x1, y1) to (x2, y2)
+void drawLine(i32 x1, i32 y1, i32 x2, i32 y2, SDL_Color *color);
+
+// Same as above, but with random generated colors
+void drawRainbowLine(i32 x1, i32 y1, i32 x2, i32 y2, u8 speed);
 
 #endif
 
